0e9d3b110035f0b0b4a5554efb68722fd4095af6
allen3325 <00857146@email.ntou.edu.tw>
2023-10-18 03:20:32 +0000

    feat: add getAllCommentList API

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import sys
 sys.path.append('word_cloud')
 from word_cloud import Word_Cloud
+sys.path.append('comment_fetcher')
+from comment_fetcher import Comment_Fetcher
 app = FastAPI()
 
 
@@ -20,3 +22,8 @@ async def generate_word_cloud(search_keyword, K: int = 100):
     print(f"search_keyword is {search_keyword}.")
     wc = Word_Cloud()
     return wc.generate_word_cloud(search_keyword, K)
+
+@app.get("/getcomment/{article_id}")
+async def generate_word_cloud(article_id: str, start: int = None, end: int = None):
+    cf = Comment_Fetcher()
+    return cf.get_comment_list(article_id, start, end)
